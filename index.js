2302ba18c64eabb5162d7849021221881e61449d
Modraut <chadmodred@gmail.com>
2021-12-02 00:41:34 +1100

    api complete

--- index.js
+++ index.js
@@ -14,9 +14,19 @@ app.use('*', function (req, res, next) {
 });
 
 app.post('/', (req, res) => {
-  return res.status(200).send("Connected")
-},
-);
+  const matches = req.body?.payload.filter(obj => obj?.drm === true && obj?.episodeCount > 0) || []
+  if (matches.length === 0) {
+    return res.status(400).send({
+      error: "Could not decode request: JSON parsing failed"
+    })
+  } else {
+    const results = matches.map(obj => {
+      const { image: { showImage = null }, slug, title } = obj
+      return { image: showImage, slug, title }
+    })
+    return res.status(200).send({ response: results })
+  }
+})
 
 
 app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`))
